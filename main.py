a3ba8dbc23716df983055b2291557815a516d8fd
Haithem Khelif <16250131+hthmkhlf@users.noreply.github.com>
2020-06-29 21:22:23 -0600

    New stuff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import praw
 import sqlite3
+import logging
 from time import  gmtime ,strftime
 
 
@@ -10,23 +11,33 @@ subreddit = reddit.subreddit("all")
 #Create the databse connections 
 db_conn = sqlite3.connect('cake.db')
 
-#Function called if the user is on his cake day it will add them to the database in order for the wishCake script to send them a happy cake day message
+#Create Logging file to track the database
+logging.basicConfig(filename='dberr.log',level=logging.DEBUG)
+
+#Add the user to the database if they are not on its
 def isCake(user:str,submission:str):
     db_cur = db_conn.cursor()
     find_user = "Select userId FROM cake_users where userID=?"
     add_user = "INSERT INTO cake_users(userID,pubID) VALUES(?,?)"
-    db_cur.execute(find_user,(user,))
-    result = db_cur.fetchone()
-    if result:
-        print("User in database")
-    else:
-        print("Not found")
+    try:
+        db_cur.execute(find_user,(user,))
+        result = db_cur.fetchone()
+        if not result:
+            db_cur.execute(add_user,(user,submission,))
+            db_conn.commit()
+            db_conn.close()
+    except sqlite3.Error as e:
+        logging.exception("Error occured on the isCake function", exc_info=True)
+    
+
 
 
 def main():
-    pass
+    for comment in subreddit.stream.comments():
+        if 
+        print(comment.subreddit )
 
 
 if __name__ == "__main__":
-    # main()
-    isCake('abc','abc')
+    main()
+    # pass
---
+++ notes.md
@@ -0,0 +1,15 @@
+# Happy cake day notes
+
+## Todo
+
+- [ ] Track user comments and posts in the past year
+- [ ] Check if the user is on cake day
+
+## Keep an eye on
+  * Excluse adult subs
+      * Exclude NSFW posts if they are the top link 
+
+
+## Track top comments and top post 
+  * Simply create two new lists one for a comment and one for a post as we are only tracking comments on SFW subs we will simply check if the comment/post we are at has a higher score if so we replace it and move on.  
+
