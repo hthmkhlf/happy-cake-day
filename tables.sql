bdb9c82ce62ed7dfd52885f9b6cf19120e7e6a50
Haithem Khelif <16250131+hthmkhlf@users.noreply.github.com>
2020-06-29 21:33:04 -0600

    New stuff

--- tables.sql
+++ tables.sql
@@ -1,3 +1,5 @@
+-- Script to simply create the needed table 
+
 CREATE TABLE cake_users(
     userID TEXT PRIMARY KEY,
     pubID TEXT,
@@ -5,8 +7,8 @@ CREATE TABLE cake_users(
     wished INT DEFAULT 0
 );
 
-ALTER TABLE users
-ADD COLUMN wished INT DEFAULT 0;
-
-
+-- Simple fake data used to test the Bot
 INSERT INTO cake_users(userID,pubID) VALUES('abcdef','abcdef')
+INSERT INTO cake_users(userID,pubID) VALUES('u8027js','t2_jki9')
+INSERT INTO cake_users(userID,pubID) VALUES('u8028js','t3_jki9')
+INSERT INTO cake_users(userID,pubID) VALUES('u4028js','t3_jli9')
